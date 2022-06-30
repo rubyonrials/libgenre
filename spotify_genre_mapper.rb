9c8174233df5ebd4c695776880e5b23d823b1f65
rubyonrials <matthew.rials+github2@gmail.com>
2022-06-29 18:22:38 -0700

    algo works after refactor

--- spotify_genre_mapper.rb
+++ spotify_genre_mapper.rb
@@ -3,7 +3,13 @@ require 'uri'
 require 'json'
 
 MIN_TRACKS_FOR_GENRE = 3
-BEARER_TOKEN = ARGV[0] || "BQCBiB6Z5C0lmNNvvbQfy2DLzBUyOnwS966C71_A6iFswmXDYs96lifYGxmkEEW3yBZIwjGzo2sFB7uf2-djexbotJ_yGBn4Eo4pGSk29lSY7dHa__j3CIwxZ7MbEil6UcQuX2PagImBXSJJ6jAxir_RjAib6-bnTJ-ytyblU_mt1DMIwoJUKxcn4UZ-nlsTt3Dx4_JzAiNGcb2UZ_AAj2ErKr9BQ9SLjlVy-w"
+if (!ARGV[0])
+  puts 'OAuth token required as an argument. Like this:'
+  puts '$ ruby spotify_genre_mapper.rb "MY_OAUTH_TOKEN_HERE"'
+  puts 'OAuth token can be found at https://developer.spotify.com/console/get-track/ by clicking "GET TOKEN"'
+  return
+end
+BEARER_TOKEN = ARGV[0]
 
 def json_request (url)
   uri = URI.parse(url)
@@ -21,12 +27,13 @@ def json_request (url)
 end
 
 def strip_tracks_response (response)
+  # Reformat response data for easier processing
   stripped_tracks = {}
   response["items"].each { |i|
     track = i["track"];
     track_id = track["id"];
-    puts track_id;
-    my_tracks[track_id] = {
+
+    stripped_tracks[track_id] = {
       name: track["name"],
       artists: track["artists"].map { |a| a["id"] },
     }
@@ -35,18 +42,31 @@ def strip_tracks_response (response)
   stripped_tracks
 end
 
-def map_genre_to_tracks (tracks)
-  genre_to_tracks = {}
-  my_tracks.keys.each { |track_id|
-    track_tuple = [track_id, my_tracks[track_id][:name]]
+def get_all_spotify_tracks
+  my_tracks_response = json_request("https://api.spotify.com/v1/me/tracks?limit=50")
+  my_tracks = strip_tracks_response(my_tracks_response)
 
-    my_tracks[track_id][:artists].each { |artist|
+  while my_tracks_response["next"] do
+    my_tracks_response = json_request(my_tracks_response["next"])
+    my_tracks = my_tracks.merge(strip_tracks_response(my_tracks_response))
+  end
+
+  my_tracks
+end
+
+def map_genre_to_tracks (tracks)
+  artist_to_genre = {} # Cache to avoide doing multiple getArtist API calls
+  genre_to_tracks = {}
+  tracks.keys.each { |track_id|
+    track_tuple = [track_id, tracks[track_id][:name]]
+
+    # Tracks don't have genre, only artists. So find all genres for all artists of the track, and add it to a map { genre => [tracks] }
+    tracks[track_id][:artists].each { |artist|
       artist_genres = nil
 
       if artist_to_genre.keys.include?(artist) # Cache hit
         artist_genres = artist_to_genre[artist]
       else
-        puts "searching artist"
         artist_genres = json_request("https://api.spotify.com/v1/artists/#{artist}")["genres"]
         artist_to_genre[artist] = artist_genres
       end
@@ -73,6 +93,7 @@ def process_genres_to_playlists (genre_to_tracks)
   playlists_to_create = {}
 
   while mutable_genre_to_tracks.keys.count > 0 do
+    # Find the genre with the most tracks
     new_genre = mutable_genre_to_tracks.keys.sort { |g1, g2|
       mutable_genre_to_tracks[g2].count <=> mutable_genre_to_tracks[g1].count
     }[0]
@@ -81,6 +102,8 @@ def process_genres_to_playlists (genre_to_tracks)
     mutable_genre_to_tracks.delete(new_genre)
     if new_genre_tracks.count >= MIN_TRACKS_FOR_GENRE
       playlists_to_create[new_genre] = new_genre_tracks
+
+      # If the genre is substantial, remove tracks tagged with that genre from other genres, to avoid duplicates
       mutable_genre_to_tracks.keys.each { |genre|
         mutable_genre_to_tracks[genre] = mutable_genre_to_tracks[genre].filter { |track |
           !new_genre_tracks.include?(track)
@@ -93,6 +116,9 @@ def process_genres_to_playlists (genre_to_tracks)
 end
 
 def find_uncategorized_tracks(my_tracks, playlists_to_create)
+  # A track is uncategorized if
+  # 1. it was made by artist(s) that Spotify has not assigned genres to
+  # 2. process_genres_to_playlists finds that the track belonged only to genres with < MIN_TRACKS_FOR_GENRE tracks
   starting_tracks = []
   my_tracks.keys.each do |track_id|
     starting_tracks << [track_id, my_tracks[track_id][:name]]
@@ -106,12 +132,16 @@ def find_uncategorized_tracks(my_tracks, playlists_to_create)
   uncategorized_tracks = starting_tracks - categorized_tracks
 end
 
-artist_to_genre = {} # Cache to avoide doing multiple getArtist API calls
-my_tracks_response = json_request("https://api.spotify.com/v1/me/tracks?limit=50")
-my_tracks = strip_tracks_response(my_tracks_response);
-genre_to_tracks = map_genre_to_tracks(my_tracks)
-playlists_to_create = process_genres_to_playlists(genre_to_tracks)
-uncategorized_tracks = find_uncategorized_tracks(my_tracks, playlists_to_create)
-playlists_to_create["uncategorized"] = uncategorized_tracks
+def main
+  my_tracks = get_all_spotify_tracks
+  genre_to_tracks = map_genre_to_tracks(my_tracks)
+  playlists_to_create = process_genres_to_playlists(genre_to_tracks)
+  uncategorized_tracks = find_uncategorized_tracks(my_tracks, playlists_to_create)
+  playlists_to_create["uncategorized"] = uncategorized_tracks
 
-# What to do about conflicting artists on a single song? SEEB remix
+  puts playlists_to_create
+  # What to do about conflicting artists on a single song? SEEB remix
+  # Could sort by descending dancability?
+end
+
+main
